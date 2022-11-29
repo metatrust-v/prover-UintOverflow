72c9d64dff47834ccd8143b3d2ef37bcb998607e
Large Zhe <92359448+wangzhecodingfy@users.noreply.github.com>
2022-11-29 20:18:07 +0800

    Add files via upload

---
+++ UintOverflow.sol
@@ -0,0 +1,11 @@
+contract _MAIN_ {
+    constructor () public {
+        uint8 a = 255;
+        uint8 b = 255;
+
+        uint8 c = a + b;
+
+        assert(c == 254);
+        assert(c == a + b);
+    }
+}
