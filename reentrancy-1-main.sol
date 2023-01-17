dbdd890cb97203a9a51e64100012cae1e4e08e80
Large Zhe <92359448+wangzhecodingfy@users.noreply.github.com>
2023-01-17 16:10:31 +0800

    Add files via upload

---
+++ reentrancy-1-main.sol
@@ -0,0 +1,54 @@
+pragma solidity ^0.8.0;
+
+contract ReentranceContract {
+
+  mapping(address => uint) public balances;
+
+  function donate(address _to) public payable {
+    balances[_to] = balances[_to] + msg.value;
+  }
+
+  function balanceOf(address _who) public view returns (uint balance) {
+    return balances[_who];
+  }
+
+  function withdraw(uint _amount) public {
+    if(balances[msg.sender] >= _amount) {
+      (bool res, ) = msg.sender.call{value:_amount}("");
+      if(res) {
+        _amount;
+      }
+      balances[msg.sender] -= _amount;
+    }
+  }
+
+  fallback() external payable {
+
+  }
+}
+
+
+// Main contract encoding the harness function as a constructor
+contract _MAIN_ {
+
+    ReentranceContract test;
+
+    constructor() public {
+      test = new ReentranceContract();
+
+      uint $amount;
+      require(address(this).balance >= $amount);
+
+      uint amount_before = address(test).balance;
+      test.donate{value: 100}(address(this));
+      test.withdraw(100);
+
+      assert(address(test).balance == amount_before);
+    }
+
+    fallback() external payable {
+      if (msg.value == 100) {
+        test.withdraw(1000);
+      }
+    }
+}
